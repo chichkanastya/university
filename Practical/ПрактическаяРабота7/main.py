a8a5cd247585d2b848f1163b8295842519ed22ae
chichkanastya <chichkanastya@gmail.com>
2025-12-23 14:47:19 +0300

    Update main.py

--- Practical/ПрактическаяРабота7/main.py
+++ Practical/ПрактическаяРабота7/main.py
@@ -9,13 +9,13 @@ content = reply.read().decode()
 name_pattern = r"class=\"org-widget-header__title-link\"[^>]*>\s*(?P<names>[^<]+)"
 street_pattern = r"<span[^>]*class=\"[^\"]*(?:street|address|location|meta-location|geo)[^\"]*\"[^>]*>\s*(?P<street>[^<\n\r]+)"
 num_pattern  = r"<dd class=[^>]+>(?P<numbers>(?:\+|7|8)[^<]+)"
-raspi_pattern = r"<dd class=\"spec__value\">(?P<raspi>[^<]*?\d{1,2}:\d{2}[^<]*)"
+table_pattern = r"<dd class=\"spec__value\">(?P<raspi>[^<]*?\d{1,2}:\d{2}[^<]*)"
 
 
 names = re.findall(name_pattern, content, re.IGNORECASE)
 streets = re.findall(street_pattern, content, re.IGNORECASE)
 phone = re.findall(num_pattern, content, re.IGNORECASE)
-table = re.findall(raspi_pattern, content, re.IGNORECASE)
+table = re.findall(table_pattern, content, re.IGNORECASE)
 
 with open('data.csv', 'w', newline='', encoding='utf-8') as file:
     writer = csv.writer(file)
@@ -32,4 +32,5 @@ with open('data.csv', 'w', newline='', encoding='utf-8') as file:
             table[i] if i < len(table) else ''
         ]
         writer.writerow(row)
+
 
